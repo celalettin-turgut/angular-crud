d2b0a3c6c05f1fe1d5bf610850436ca0f48c4a9d
Celalettin Turgut <celalettin.turgut@hotmail.com>
2022-04-27 00:56:32 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'angular-crud';
+export class AppComponent implements OnInit {
+  posts? = [];
+  todos? = [];
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    const posts = fetch('https://jsonplaceholder.typicode.com/posts')
+      .then((posts) => posts.json())
+      .then((posts) => {
+        console.log(posts);
+        this.posts = posts;
+      });
+
+    this.http
+      .get('https://jsonplaceholder.typicode.com/todos')
+      .subscribe((todos) => this.todos);
+  }
+
+  click() {}
 }
